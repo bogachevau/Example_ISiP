d9dc02b5eaf6e0c345048a8ebb246e9ac584af96
Alexandr <alexandr.bogachev@gmail.com>
2022-10-28 13:47:59 +0300

    Добавил класс Cat

---
+++ src/main/java/Examples/Cat.java
@@ -0,0 +1,7 @@
+package Examples;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/Examples/Test.java
+++ src/main/java/Examples/Test.java
@@ -14,22 +14,22 @@ public class Test {
     public static void main(String[] args) {
         Person[] people = new Person[5];
         people[0] = new Person("Ivanov Ivan", "Engineer",
-                "ivanov@mail.ru", "79514532681", 40000, 32);
+                "ivanov@mail.ru", "79514532681", 40000, 42);
         people[1] = new Person("Semenov Semen", "Product manager",
                 "semenov@mail.ru", "7111111111", 50000, 28);
         people[2] = new Person("Petrov Petr", "Developer",
                 "petrov@mail.ru", "72222222222", 60000, 35);
         people[3] = new Person("Smirnov Oleg", "Developer",
-                "smirnov@mail.ru", "73333333333", 45000, 25);
+                "smirnov@mail.ru", "73333333333", 45000, 41);
         people[4] = new Person("Ivanova Anna", "Designer",
                 "ivanova@mail.ru", "74444444444", 52000, 27);
 
         for (Person person : people) {
-            if (person.getAge() < 30) {
+            if (person.getAge() > 40) {
                 person.print();
             }
         }
-
+        System.out.println("--------------------------");
         for (Person person : people) {
             if (person.getPayday() > 45000) {
                 person.print();
